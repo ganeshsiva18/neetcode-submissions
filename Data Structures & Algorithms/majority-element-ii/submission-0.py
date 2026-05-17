79773c45c09a7f73097c98ffb33b151943557f28
Ganesh Sivadasan <103896643+ganeshsiva18@users.noreply.github.com>
2026-05-17 09:47:09 -0400

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        res, n = [], len(nums)
+
+        i = 0
+        while i < n:
+            j = 1 + i
+            while j < n and nums[i] == nums[j]:
+                j += 1
+            if (n // 3 < j - i):
+                res.append(nums[i])
+            i = j
+        return res
+                
+
